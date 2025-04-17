f0c9e4eff4b930ad6219176db68cb645f9a9c47e
awatersny <awatersny@gmail.com>
2025-04-17 17:21:34 -0400

    Reinstate global error handling

--- server.mjs
+++ server.mjs
@@ -16,9 +16,9 @@ app.use("/grades", grades);
 app.use("/grades", grades_agg);
 
 // Global error handling
-// app.use((err, _req, res, next) => {
-//   res.status(500).send("Seems like we messed up somewhere...");
-// });
+app.use((err, _req, res, next) => {
+  res.status(500).send("Seems like we messed up somewhere...");
+});
 
 // Start the Express server
 app.listen(PORT, () => {
